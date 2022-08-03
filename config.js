054459cefdd3b1daafe8bd1db2fa234a3ea288f9
Santi Miranda <santimir@protonmail.com>
2022-08-03 15:31:03 +0200

    tidied up

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export default { 
+const config { 
   importMeta: import.meta, 
   flags:{
     polynomy:{type:'string', alias:'p'},//write polynomy as csv string, low to high
@@ -6,3 +6,28 @@ export default {
     message: {type:'string', alias:'m'}//message from user
     }
   }
+
+
+//import selectByGrade from 'wikiPolys'; (just an idea, we create an object with the ones w maximum
+//period)
+
+
+//set up of the CLI parser
+const { message, polynomial, grade } = meow("parsing...", config).flags
+
+if(!message){
+  throw new SyntaxError("User message is missing.")
+}
+
+if(!p && !grade){
+  throw new SyntaxError("Either polynomyal or grade must be specified.")
+}
+
+const binaryMsg = message
+.split('')
+.map(letter => { 
+    const binaryLetter = letter.charCodeAt().toString(2);
+    return parseInt(binaryLetter,2)
+    })
+
+export default { binaryMsg, polynomial, grade }
--- lsfr.js
+++ lsfr.js
@@ -1,59 +1,30 @@
 #!/usr/bin/env node
 
 //LSFR partial, silly, slow, wrong etc. try in Javascript
+//import selectByGrade from 'wikiPolys'; // (idea)
+import { binaryMsg, polynomy:p, grade } from "./config.js";
 
-import meow from 'meow';
-import meowConfig from './config.js';
-//import selectByGrade from 'wikiPolys'; (just an idea, we create an object with the ones w maximum
-//period)
+let polynomial;
 
+if( grade ) polynomial = selectByGrade[`${grade}`]
 
-//set up of the CLI parser
-const args = meow("parsing...", config)
+else if( p ) poly = polynomyal.split(',').map(str=>parseInt(str,2))
 
-//get the flags
-const { message, polynomy, grade } = args.flags
-
-// set up the method
-if(!message){
-throw new SyntaxError("User message is missing.")
-}
-
-if(!polynomy && !grade){
-throw new SyntaxError("Either polynomyal or grade must be specified.")
-}
-
-// we may gather them from wikipedia (hardcoded now)
-let poly;
-
-if(grade) poly = selectByGrade[`${grade}`]
-
-else if(polynomyal) poly = polynomyal.split(',').map(str=>parseInt(str,2))
-
-// binaryMsg will be an array of bits from the original message
-const binaryMsg = msg
-.split('')
-.map(letter => { 
-const binaryLetter = letter.charCodeAt().toString(2);
-return parseInt(binaryLetter,2)
-})
 
 console.log(binaryMsg)// logs in decimal
 
-let key = initialRandomBinaryKey(polynomy.length - 1)
+let key = initialRandomBinaryKey(polynomial.length - 1)
 
-// now we have the initial state S, P, and  the message
-
-// we need to extend S to binaryMsg.length or encode with it if it is shorter
+  // extend key to binaryMsg.length or encode with it if it is shorter
 
 if(binaryMsg.length < key.length){
- //encode bit by bit using bitwise operators
- // function 'binary encrypt'
- }
+  //encode bit by bit using bitwise operators
+  // function 'binary encrypt'
+}
 
 else {
-// extend 'key' to length of 'binaryMsg'
-// for this we do a vector vector product of P * S.slice[0,P.length]
-// and place the result first in the key
-//'binary encrypt'
+  // extend 'key' to length of 'binaryMsg'
+  // for this we do a vector vector product of P * S.slice[0,P.length]
+  // and place the result first in the key
+  //'binary encrypt'
 }
