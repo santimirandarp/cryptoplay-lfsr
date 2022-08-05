2c29f9580be91dd908661409572db37bb6da7367
Santi Miranda <santimir@protonmail.com>
2022-08-05 23:21:56 +0200

    refactor: moved some extra stuff to main bin

--- lfsr.js
+++ lfsr.js
@@ -2,32 +2,43 @@
 
 // Beginner implementation of lfsr in javascript
 
-import { binaryCharCodes, coefficients, key } from "./local_modules/set-up.js";
-import { extendKey, encryptDecrypt } from "./local_modules/utils.js";
+import { message, coefficients, key } from "./local_modules/set-up.js";
+import {
+  extendKey,
+  encryptDecrypt,
+  setKeyFromInput,
+  textToCharCodes,
+  charCodeToBinaryArray,
+} from "./local_modules/utils.js";
 
 /** stream-cipher using lfsr as PRNG */
-export function lfsrStreamCipher(binaryCharCodes, key, coefficients){
+export function lfsrStreamCipher(message, key, coefficients) {
+  let result;
 
-let result;
+  /** set up the keys binary array */
+  key = setKeyFromInput(key, coefficients.length);
 
-let msgBitLength = 0;
-binaryCharCodes.forEach(symbol => msgBitLength += symbol.length)
+  /* get the message to binary form */
+  let msgBitLength = 0;
+  const binaryCharCodes = textToCharCodes(message).map((cc) =>
+    charCodeToBinaryArray(cc)
+  );
+  binaryCharCodes.forEach((symbol) => (msgBitLength += symbol.length));
 
-// 1. Reversing key and coefficients wont alter the network (and the output bit)
-// 2. newBit is pushed to the array.
-key = key.reverse()
-if (msgBitLength <= key.length) {
-
-  result = encryptDecrypt(binaryCharCodes, key);
-
-} else {
-
-  const cfs = coefficients.reverse()
-  const extendedKey = extendKey(key, cfs, msgBitLength)
-  result = encryptDecrypt(binaryCharCodes, extendedKey) 
-
+  // 1. Reversing key and coefficients wont alter the network (and the output bit)
+  // 2. newBit is pushed to the array.
+  key = key.reverse();
+  if (msgBitLength <= key.length) {
+    result = encryptDecrypt(binaryCharCodes, key);
+  } else {
+    const cfs = coefficients.reverse();
+    const extendedKey = extendKey(key, cfs, msgBitLength);
+    result = encryptDecrypt(binaryCharCodes, extendedKey);
+  }
+  return result;
 }
-  return result
-}
 
-console.log(lfsrStreamCipher(binaryCharCodes, key, coefficients))
+const encrypt = lfsrStreamCipher(message, key, coefficients);
+const decrypt = lfsrStreamCipher(encrypt, key, coefficients);
+
+console.log(encrypt, decrypt);
--- local_modules/set-up.js
+++ local_modules/set-up.js
@@ -2,17 +2,12 @@ import meow from "meow";
 
 import config from "./meow-config.js";
 import polynomials from "./wikiPolys.js";
-import {
-  textToCharCodes,
-  charCodeToBinaryArray,
-  setKeyFromInput,
-  binaryStringToBinaryArray,
-} from "./utils.js";
+import { binaryStringToBinaryArray } from "./utils.js";
 
 const flags = meow(config).flags;
 
 let { coefficients, key } = flags;
-const { relax, message, degree } = flags;
+const { /*relax,*/ message, degree } = flags;
 
 /** set up the coefficients binary array */
 if (degree && degree <= 24 && degree >= 2) {
@@ -26,12 +21,6 @@ if (degree && degree <= 24 && degree >= 2) {
   throw new Error("csv-coefficients or degree must be specified.");
 }
 
-/** set up the keys binary array */
-key = setKeyFromInput(key, coefficients.length, relax);
-
-const binaryCharCodes = textToCharCodes(message).map((cc) =>
-  charCodeToBinaryArray(cc)
-);
 // array of array of symbols as binary;
 
-export { binaryCharCodes, coefficients, key };
+export { message, coefficients, key };
